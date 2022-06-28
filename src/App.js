bb12a8916d63db12053c31062355c977feb77888
Nilamadhab <nilamadhab47@gmail.com>
2022-06-28 22:19:58 +0530

    font change, social media buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import  axios  from 'axios';
 import React, { Component } from 'react'
 import "./App.css";
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  PinterestShareButton,
+  RedditShareButton,
+  TelegramShareButton,
+  TwitterShareButton,
+} from "react-share";
+import {
+  FacebookIcon,
+  LinkedinIcon,
+  PinterestIcon,
+  RedditIcon,
+  TelegramIcon,
+  TwitterIcon,
+} from "react-share";
 
 
 
@@ -29,6 +45,7 @@ class App extends Component {
 
   render() {
     const {advice} = this.state
+    console.log(advice)
 
     return (
       <div className= "app">
@@ -39,6 +56,62 @@ class App extends Component {
               GET A ADVICE !!
             </span>
           </button>
+          <div className="share">
+            <FacebookShareButton
+            url={`Visit For more ${"https://silver-sable-e2cbe8.netlify.app/"}`}
+
+            quote= {`quote of the day ${advice} `}
+            hashtag={advice}
+            >
+              <FacebookIcon size={32} round={true}/>
+
+            </FacebookShareButton>
+            <TwitterShareButton
+            url={`Visit For more ${"https://silver-sable-e2cbe8.netlify.app/"}`}
+
+            title={advice} 
+            via = "NilamadhabSenap"
+            hashtags={["quotes",]}
+            related={["@NilamadhabSenap"]}
+
+            >
+              <TwitterIcon size={32} round={true} />
+
+            </TwitterShareButton>
+            <PinterestShareButton
+            url={`Visit For more ${"https://silver-sable-e2cbe8.netlify.app/"}`}
+            media={advice}
+            
+            >
+              <PinterestIcon size={32} round={true} />
+
+            </PinterestShareButton>
+            <LinkedinShareButton
+            url={`Visit For more ${"https://silver-sable-e2cbe8.netlify.app/"}`}
+            title='quotes for the day'
+            summary={advice}
+            source={`Visit For more ${"https://silver-sable-e2cbe8.netlify.app/"}`}
+            >
+              <LinkedinIcon size={32} round={true} />
+            </LinkedinShareButton>
+            <RedditShareButton
+            url={`Visit For more ${"https://silver-sable-e2cbe8.netlify.app/"}`}
+            title={advice}
+            >
+              <RedditIcon size={32} round={true} />
+
+            </RedditShareButton>
+            <TelegramShareButton
+            url={`Visit For more ${"https://silver-sable-e2cbe8.netlify.app/"}`}
+            title={advice}
+            >
+              <TelegramIcon size={32} round={true} />
+
+            </TelegramShareButton>
+
+
+          </div>
+
         </div>
       </div>
     )
